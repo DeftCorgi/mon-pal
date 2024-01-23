65a6879dac003719bb784d8ffbd5783e92aa5565
Toan Au <toanau.dev@gmail.com>
2024-01-24 01:35:09 +1100

    added DB logging

--- db/sequelize.config.js
+++ db/sequelize.config.js
@@ -5,6 +5,18 @@ const dbPath = "./db/mhw.db";
 const sequelizeConfig = new Sequelize({
   dialect: "sqlite",
   storage: dbPath,
+  logging: (log) => {
+    console.log(log); // Log the Sequelize query and other information
+  },
 });
+
+sequelizeConfig
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 export default sequelizeConfig;
